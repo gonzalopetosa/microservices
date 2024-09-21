c6c29bb7ca03e4dd2b4643989a8c460c25fc372d
gonzalopetosa <gonzalopetosa1234@gmail.com>
2024-09-20 22:08:39 -0300

    creacion del miroservicio booking

---
+++ boocking/src/main/java/com/gonza/boocking/BoockingApplication.java
@@ -0,0 +1,13 @@
+package com.gonza.boocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoockingApplication.class, args);
+	}
+
+}
---
+++ boocking/src/test/java/com/gonza/boocking/BoockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonza.boocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
