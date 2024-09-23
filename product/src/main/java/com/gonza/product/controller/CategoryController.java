11506bc06a5cb7bd833e70adfc51d763b3dff9f0
gonzalopetosa <gonzalopetosa1234@gmail.com>
2024-09-23 18:27:35 -0300

    configuacion para hacer actualizar automaticamente las propiedades del repo git

---
+++ product/src/main/java/com/gonza/product/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package com.gonza.product.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@RestController
+@RequestMapping("/api/category")
+@Getter
+@Setter
+@RefreshScope
+public class CategoryController {
+	
+	@Value("${app.testProp}")
+	private String testProp;
+	
+	@GetMapping("/test-prop")
+	public String getTestProp() {
+		return testProp;
+	}
+
+}
